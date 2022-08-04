8a51f5da01ba7404cdf75ccb8cd5118b52c953c3
Tobi <tobi2045@outlook.com>
2022-08-04 17:56:50 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3{
+  public static void main(String[] args){
+    int birthYear = 1995;
+    String birthMonth = "June";
+    boolean isWeekend = false;
+
+    System.out.println(birthYear);
+    System.out.println(birthMonth);
+    System.out.println(isWeekend);
+
+
+  }
+}
