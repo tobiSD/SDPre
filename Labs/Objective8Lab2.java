fe80572276ad95aa5ce63bb46c5e59871b3fbf7b
Tobi <tobi2045@outlook.com>
2022-08-05 14:03:26 -0600

    complete objective8lab2a

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2{
+  public static void main(String[] args) {
+
+    int sum=0;
+    int i;
+
+    for(i=0; i<=20; i++){
+      sum = i + sum;
+    }
+    System.out.println(sum);
+  }
+}
