e38fa831306646508236b554accce102f5331574
Tobi <tobi2045@outlook.com>
2022-08-04 21:39:55 -0600

    complete objective3ab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2{
+  public static void main(String[] args){
+    double side1, side2, hypotenuse;
+
+    side1 = 10.0 * 10.0;
+    side2 = 8.0 * 8.0;
+    hypotenuse = Math.sqrt (side1 + side2);
+
+    System.out.println("The hypotenuse of a triangle with sides 10.0 and 8.0 is " + hypotenuse);
+  }
+}
